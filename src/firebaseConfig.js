b1e63f1a72f404fdf7e5cb7b9dd5ebbf52147d25
suryaprabha2408-code <suryaprabha2408@gmail.com>
2026-04-01 15:43:32 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,8 +1,5 @@
-// src/firebaseConfig.js
-
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; // 👈 Indha line irukkanum
-
+import { getFirestore } from "firebase/firestore"; 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "civic-eye-app1.firebaseapp.com",
@@ -11,9 +8,5 @@ const firebaseConfig = {
   messagingSenderId: "1097393055074",
   appId: "1:1097393055074:web:78276d585f1f32d545383b"
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// 👇 Indha line romba mukkiyam! Ithu illana error varum.
 export const db = getFirestore(app);
