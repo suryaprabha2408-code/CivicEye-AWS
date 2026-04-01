a781d7f868bc4bae53390326837d9a6eff42c52a
suryaprabha2408-code <suryaprabha2408@gmail.com>
2026-04-01 15:34:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-// 👇 Indha IMPORT mukkiyam!
 import AdminJobs from './AdminJobs'; 
-
 import LandingPage from './LandingPage';
 import Login from './Login';
 import Dashboard from './Dashboard';
 import Voting from './Voting';
 import JobsPage from './JobsPage';
-import TransportPage from './TransportPage'; // <-- Import pannanum
-import NewsPortal from './NewsPortal'; // Import pannunga
+import TransportPage from './TransportPage'; 
+import NewsPortal from './NewsPortal'; 
 import SuperAdmin from './SuperAdmin';
 import About from './About';
 import Departments from './Departments';
@@ -21,14 +18,13 @@ function App() {
       <Routes>
         {/* Main website link */}
         <Route path="/" element={<LandingPage />} />
-
         {/* Login Page */}
         <Route path="/login" element={<Login />} />
 
-        {/* 🚧 ROADS/ELECTRICITY Admin Dashboard */}
+        {/*  ROADS/ELECTRICITY Admin Dashboard */}
         <Route path="/dashboard" element={<Dashboard />} />
 
-        {/* 💼 JOBS Admin Dashboard (NEW) */}
+        {/* JOBS Admin Dashboard (NEW) */}
         <Route path="/admin" element={<AdminJobs />} />
 
         {/* Other Pages */}
@@ -37,7 +33,6 @@ function App() {
         <Route path="/transport" element={<TransportPage />} />
         <Route path="/news" element={<NewsPortal />} />
         <Route path="/super-admin" element={<SuperAdmin />} />
-        {/* Puthiya Routes */}
         <Route path="/about" element={<About />} />
         <Route path="/departments" element={<Departments />} />
         <Route path="/privacy" element={<PrivacyPolicy />} />
